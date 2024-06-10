e181959b5139926bead0e4a7756deb9c80c2bc80
LiridonOlluri <Liridonn.olluri@gmail.com>
2024-06-10 23:49:17 +0200

    update stuff

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,47 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-export const formatPrice = (price: number) => {
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  })
 
-  return formatter.format(price)
+export const formatPrice = (price: number) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  return formatter.format(price);
+};
+
+export function constructMetadata({
+  title = "DaxCases - custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/public/dax.jpg",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@daxcases",
+    },
+    icons,
+    metadataBase: new URL("https://daxcases.vercel.app/"),
+  };
 }
